83fb518d3808937febb408915c3fd94773ddab83
sanyokstd <59456430+sanyokstd@users.noreply.github.com>
2020-10-06 13:05:38 +0300

    1
    1

--- build/js/main.js
+++ build/js/main.js
@@ -1,4 +1,6 @@
 $(function(){
+
+	//слайдер на главной
 	$('.slides').slick({
 		arrows: true,
 		fade: true,
@@ -13,16 +15,61 @@ $(function(){
 	    ]
 	})
 
+	$('.slider_priyt').slick({
+ 		slidesToScroll: 1,
+ 		variableWidth: true,
+ 		responsive: [
+	    {
+	      breakpoint: 767,
+	      settings: {
+	       variableWidth: false,
+	       slidesToShow: 1,
+	      }
+	    },
+	    ]
+	})
 
+	//поиск на мобильном
 	$('.search_mob').click(function(){
 		event.preventDefault();
 		$('.search_mob .sear').fadeToggle()
 		return false;
 	})
 
+	//Бургер меню
 	$('a[href="#menu"]').click(function(){
 		event.preventDefault();
 		$(this).toggleClass('active')
 		$('#menu').toggleClass('active')
 	})
+
+	//смега фасовки
+	$('.fasovka__item').click(function(){
+		$(this).parent().find('.fasovka__item').removeClass('active')
+		$(this).addClass('active')
+	})
+
+	//
+	$('.like').click(function(){
+		$(this).toggleClass('wish_added')
+	})
+
+
+	$('.sidebar1 .title').click(function(){
+		$(this).next('.sidebarnav').slideToggle()
+	})
+	$('.sidebar2 .title').click(function(){
+		$(this).next('.sidebarnav').slideToggle()
+	})
+
+	$('.footer-block h3').click(function(){
+		if($(this).hasClass('active')){
+			$(this).removeClass('active')
+		}
+		else{
+			$('.footer-block h3').removeClass('active')
+			$(this).addClass('active')
+		}
+	})
+
 })
